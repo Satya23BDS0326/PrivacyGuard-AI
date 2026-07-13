ddf339c31effe234b873a33b6840aeef506115e6
Balla Satya Narayana <balla.veera2023@vitstudent.ac.in>
2026-07-13 20:57:11 +0530

    Fix typo in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from langchain_Openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import strOutputParser
-hftujrjrtjj
+hftujrjrtj
 
 import streamlit as st 
 import os 
